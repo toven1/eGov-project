d464836973b370cc67cd458b63cc5f99f16e18e9
현경 <er041356@naver.com>
2025-02-17 16:16:14 +0900

    entity

--- src/main/java/org/example/entity/student/StudentActive.java
+++ src/main/java/org/example/entity/student/StudentActive.java
@@ -9,10 +9,10 @@ public class StudentActive {
     private String rrn;
     private String phone;
     private String address;
-    private int student_number;
+    private int studentNumber;
     private Status status;
-    private String admission_type;
-    private int academic_year;
+    private String admissionType;
+    private int academicYear;
     private int semester;
     private String faculty;
     private String department;
@@ -37,15 +37,30 @@ public class StudentActive {
     public String getAddress() {return address;}
     public void setAddress(String address) {this.address = address;}
 
-    public int getStudent_number() {return student_number;}
-    public void setStudent_number(int student_number) {this.student_number = student_number;}
+    public int getStudent_number() {return studentNumber;}
+    public void setStudent_number(int student_number) {this.studentNumber = student_number;}
 
     public Status getStatus() {return status;}
     public void setStatus(Status status) {this.status = status;}
 
-    public String getAdmission_type() {return admission_type;}
-    public void setAdmission_type(String admission_type) {this.admission_type = admission_type;}
+    public String getAdmission_type() {return admissionType;}
+    public void setAdmission_type(String admission_type) {this.admissionType = admission_type;}
 
-    public int getAcademic_year() {return academic_year;}
+    public int getAcademic_year() {return academicYear;}
     public void setAdmission_date(){this.admission_date = LocalDate.parse(admission_date.toString());}
+
+    public int getSemester() {return semester;}
+    public void setSemester(int semester) {this.semester = semester;}
+
+    public String getFaculty() {return faculty;}
+    public void setFaculty(String faculty) {this.faculty = faculty;}
+
+    public String getDepartment() {return department;}
+    public void setDepartment(String department) {this.department = department;}
+
+    public String getClass_group() {return class_group;}
+    public void setClass_group(String class_group) {this.class_group = class_group;}
+
+    public LocalDate getAdmission_date() {return admission_date;}
+    public void setAdmission_date(LocalDate admission_date) {this.admission_date = admission_date;}
 }
