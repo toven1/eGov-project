400ac9953f85f64beb4d13f50ef8cef6fff5a2f3
현경 <er041356@naver.com>
2025-02-18 21:24:13 +0900

    login 로그 남기는 로직 입니다.

--- src/main/java/org/example/controller/LoginController.java
+++ src/main/java/org/example/controller/LoginController.java
@@ -1,5 +1,7 @@
 package org.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.example.entity.student.StudentActive;
 import org.example.service.student.StudentActiveService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,8 @@ import org.springframework.ui.Model;
 @Controller
 public class LoginController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private StudentActiveService studentService;
 
@@ -27,9 +31,13 @@ public class LoginController {
     public String login(@RequestParam StudentActive studentNumber, @RequestParam String password, Model model){
         boolean isAuthenticated = studentService.login(studentNumber,password);
         if(isAuthenticated){
+            logger.info("Studentname: {} Login Successful", studentNumber);
             model.addAttribute("stNumber",studentNumber);
             return "main";
         }else{
+            //실패한 로그를 남김 입력한 학번을 알수 있음
+            logger.info("Studentname: {} Login Failed", studentNumber);
+
             //로그인 정보가 일치 않다는걸 이벤트 메세지로 띄움
             model.addAttribute("loginErrorMessage","회원정보가 일치하지 않습니다.");
             return "login";
